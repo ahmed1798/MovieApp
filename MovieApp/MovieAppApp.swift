4b14b9bceed198058f31f5960c03368bc09d28a8
eissa <ahmedeissa815@2grand.net>
2024-09-29 19:05:55 +0400

    Initial Commit

---
+++ MovieApp/MovieAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  MovieAppApp.swift
+//  MovieApp
+//
+//  Created by Ahmed Eissa on 29/09/2024.
+//
+
+import SwiftUI
+
+@main
+struct MovieAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
